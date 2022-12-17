b4db9633ed2f06ec4c66048ebf7897115ee0af7c
Federico A. Corazza <federico.a.corazza@gmail.com>
2022-12-17 20:08:57 +0100

    Move utils.nim under layers

--- src/layers/utils.nim
+++ src/layers/utils.nim
@@ -1,13 +1,3 @@
-import std/streams
-
-proc newEIO*(msg: string): ref IOError =
-  new result
-  result.msg = msg
-
-proc read*[T](s: Stream, result: var T) =
-  if readData(s, addr(result), sizeof(T)) != sizeof(T):
-    raise newEIO("Cannot read from stream")
-
 const Printables = {' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'}
 
 proc dataString*(data: openArray[uint8]): string =

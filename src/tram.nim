3d0fa05f867c03048d7fef82df2cadf6829c2f37
Federico A. Corazza <federico.a.corazza@gmail.com>
2023-01-05 22:50:11 +0100

    Export toAscii function

--- src/tram.nim
+++ src/tram.nim
@@ -2,6 +2,7 @@ import streams/[packets, global_header]
 import layers/link/ethertypes
 import layers/internet/[ipv4, ip_protocols]
 import layers/transport/tcp
+import layers/utils
 
 export initPacketStream, getPackets
 export GlobalHeader
@@ -9,3 +10,4 @@ export GlobalHeader
 export EtherTypes
 export Ipv4Address, Ipv4Packet, IpProtocols, parseIpv4, parseIpv4Address
 export TcpPacket, parseTcp
+export toAscii
